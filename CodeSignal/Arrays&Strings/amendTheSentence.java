ef21ea15f4f31735639a2de068defbda16fdbc35
GioLucero <gioandrewlucero@gmail.com>
2020-09-18 14:29:43 -0400

    Added code signal folders.

---
+++ CodeSignal/Arrays&Strings/amendTheSentence.java
@@ -0,0 +1,27 @@
+// You have been given a string s, which is supposed to be a sentence. 
+// However, someone forgot to put spaces between the different words, 
+// and for some reason they capitalized the first letter of every word. 
+// Return the sentence after making the following amendments:
+
+// Put a single space between the words.
+// Convert the uppercase letters to lowercase.
+
+// Ex:
+// input: 'CodeIsAwesome'
+// output: 'Code Is Awesome'
+
+String amendTheSentence(String s) {
+    // Initialize hashmap
+    // Iterate through array, and store values in hashmap
+    // Check for Capital letters following the first
+        // Insert a space before capital 
+    // return new array
+    int n = s.length();
+
+    HashMap<String, Integer> newArr = new HashMap<>();
+
+    for (int i=0; i<n; i++) {
+        
+    }
+    
+}
